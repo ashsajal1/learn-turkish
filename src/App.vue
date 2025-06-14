596b5de1a8ab1d453989cb3e562ae0c2a349406c
Ashsajal <ashsajal1@gmail.com>
2025-06-14 21:55:01 +0600

    style: add margin bottom

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <Navbar />
-    <main class="p-2 flex-grow">
+    <main class="p-2 flex-grow mt-[60px]">
       <RouterView />
     </main>
     <Footer />
